ad38a64e8396d47efa3ce1903bc279a8c90a4556
brentvanhese <r0903817@student.thomasmore.be>
2021-12-13 17:49:20 +0100

    CommandWord invullen

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -1,2 +1,18 @@
-public class CommandWord {
+/**
+ * Alle geldige opdrachtwoorden voor het spel.
+ */
+public enum CommandWord {
+    GO("go"), QUIT("quit"), HELP("?"), UNKNOWN(""), TAKE("take"), DROP("drop"), LOOK("look"), EAT("eat");
+
+    private String word;
+
+    CommandWord(String word){
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
 }
+
+
