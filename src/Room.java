7b6a94e706802b25a4fb77a0055124059df62f21
brentvanhese <r0903817@student.thomasmore.be>
2021-12-13 17:53:32 +0100

    Room invullen

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,100 @@
-public class Room {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ *
+ * @author  Michael Kölling and David J. Barnes
+ * @version 2011.07.31
+ */
+
+public class Room
+{
+    private String description;
+    public HashMap<String, Room> exits;
+    private ArrayList<Item> items;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    public Room(String description)
+    {
+        this.description = description;
+        exits = new HashMap<>();
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item){
+        items.remove(item);
+    }
+
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public String getLongDescription() {
+        String info =  description;
+        if (!items.isEmpty()){
+            info += ". This room contains items:";
+            for (Item item : items) {
+                info += item.toString();
+            }
+            info += "\n";
+        }
+        info += "\n" + getExitString();
+        return info;
+    }
+
+    public void setExit(String direction, Room room){
+        exits.put(direction, room);
+    }
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+
+    public String getExitString(){
+        String exitString = "Exits: ";
+        for (String direction : exits.keySet()){
+            exitString += direction + " ";
+        }
+        return exitString;
+    }
+
+    public boolean hasItem(String name){
+        for (Item item : items) {
+            if (item.getName().equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Item getItem(String itemName){
+        for (Item item : items) {
+            if (item.getName().equals(itemName)){
+                return item;
+            }
+        }
+        return null;
+    }
 }
